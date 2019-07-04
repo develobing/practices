ff67f32f2d96a7feb78b8e417d456683ddfc05c3
Robin <philoin88@gmail.com>
2019-07-05 01:08:51 +0900

    bcrypt로 password 해시화 및 Login 기능 추가

--- node_jwt/routes/auth.js
+++ node_jwt/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../model/User')
-const { registerValidation } = require('../validation')
+const bcrypt = require('bcryptjs')
+const { registerValidation, loginValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
   // Validate the data beofre we make a new user
@@ -15,23 +16,43 @@ router.post('/register', async (req, res) => {
     return res.status(400).send('Email already exists')
   }
 
+  // Hash the passwords
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
   // Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   })
 
   try {
     const savedUser = await user.save()
-    res.send(savedUser)
+    res.send({ user: user._id })
+    //res.send(savedUser)
   } catch(err) {
     res.status(400).send({ message: err })
   }
 })
 
-router.post('/login', (req, res) => {
-  res.send('')
+// Login
+router.post('/login', async (req, res) => {
+  // Validate the data before loging in
+  const { error } = loginValidation(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
+
+  // Checking if the user is in the databases
+  const user = await User.findOne({ email: req.body.email })
+  if(!user) {
+    return res.status(400).send('Email is wrong')
+  }
+
+  // Check Password is correct
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+  if(!validPassword) return res.status(400).send('Password is wrong')
+
+  res.send('Login is Success')
 })
 
 module.exports = router;
